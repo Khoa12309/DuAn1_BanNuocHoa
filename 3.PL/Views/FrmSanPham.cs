850f2b70bd2e50ac49e583bdbf81baf6e394efe0
Xuân Khoa <vukhoa20122003@gmail.com>
2022-11-30 15:54:26 +0700

    codeSanPham

---
+++ 3.PL/Views/FrmSanPham.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3.PL.Views
+{
+    public partial class FrmSanPham : Form
+    {
+        public FrmSanPham()
+        {
+            InitializeComponent();
+        }
+        private byte[] epbyte(PictureBox pc)
+        {
+            MemoryStream ms= new MemoryStream();
+            pcb_anh.Image.Save(ms, pcb_anh.Image.RawFormat);
+            return ms.ToArray();
+        }
+
+        private void btn_chonAnh_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Title = "Chọn Hình Ảnh";
+            opf.Filter = "Image Files (*.gif;*.jpg;*.png)|*.gif;*.jpg;*.png";
+            if (opf.ShowDialog() == DialogResult.OK)
+            {
+                pcb_anh.ImageLocation = opf.FileName;
+            }
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
