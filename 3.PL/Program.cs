143e442f28f23252a392c38fd72a6e0236183ffd
Dungndph20833 <118423397+honeybabyhhhhhh@users.noreply.github.com>
2022-12-07 21:44:04 +0700

    l

--- 3.PL/Program.cs
+++ 3.PL/Program.cs
@@ -20,7 +20,7 @@ namespace _3.PL
             Application.SetCompatibleTextRenderingDefault(false);
             //   Application.Run(new FrmDangNhap());
            //  A/pplication.Run(new FrmHangSp());
-             Application.Run(new FrmBanHang());
+             Application.Run(new Form1());
            //  Application.Run(new FrmLoaiSp());
             
 
